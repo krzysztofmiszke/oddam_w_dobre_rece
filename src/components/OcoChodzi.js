7a5be644e59e2bd44a5e2acff170dea54d9bca26
Krzysztof <krzysztof.miszke@gmail.com>
2020-01-13 23:45:27 +0100

    #1d done

--- src/components/OcoChodzi.js
+++ src/components/OcoChodzi.js
@@ -4,12 +4,34 @@ import React, { Component } from 'react';
 //   BrowserRouter as Router,
 //   Route
 // } from 'react-router-dom';
-// import Link from 'react-scroll'
+import { Link } from 'react-router-dom';
 
 class OcoChodzi extends Component {
     render() {
         return (
-            <div id='o-co-chodzi'>O co chodzi</div>
+            <div id='o-co-chodzi'>
+                <h2>Wystarczą 4 proste kroki</h2>
+                <div className='decoration-image'></div>
+                <div className='icons'>
+                    <div className='first-icon'>
+                        <div className='first-icon-bg'></div>
+                        <div>Wybierz rzeczy<hr></hr>Ubrania, zabawki, sprzęt i inne</div>
+                    </div>
+                    <div className='second-icon'>
+                        <div className='second-icon-bg'></div>
+                        <div>Spakuj je<hr></hr>skorzystaj z worków na śmieci</div>
+                    </div>
+                    <div className='third-icon'>
+                        <div className='third-icon-bg'></div>
+                        <div>Zdecyduj komu chcesz pomóc<hr></hr>wybierz zaufane miejsce</div>                   
+                    </div>
+                    <div className='fourth-icon'>
+                        <div className='fourth-icon-bg'></div>
+                        <div>Zamów kuriera<hr></hr>kurier przyjedzie w dogodnym terminie</div>                 
+                    </div>
+                </div>
+                <Link to='/logowanie' className='main-button'>ODDAJ <br/> RZECZY</Link>
+            </div>
         )
     }
 }
